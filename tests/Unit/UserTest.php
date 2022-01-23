980b3769d3f7e4cbcd9431f8fb1191c949a4552e
ben.furfie <hello@benfurfie.co.uk>
2022-01-23 16:18:49 +0000

    #1 Add basis of user profile

--- tests/Unit/UserTest.php
+++ tests/Unit/UserTest.php
@@ -1,10 +1,16 @@
 <?php
 
 use App\Models\User;
+use App\Models\UserProfile;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
-// test('a user has a profile', function () {
-//     // Given
-//     $user = User::factory()->create();
+uses(RefreshDatabase::class);
 
-//     $this->assertInstanceOf(Collection::class, $user->profile);
-// });
+test('a user has a profile', function () {
+    $user = User::factory()->create();
+    $userProfile = UserProfile::factory([
+        'user_id' => $user->id,
+    ])->create();
+
+    $this->assertInstanceOf(UserProfile::class, $user->profile);
+});
