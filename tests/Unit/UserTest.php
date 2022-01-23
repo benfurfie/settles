2691e11501c77ea6475b6d26a36882e4f140b787
ben.furfie <hello@benfurfie.co.uk>
2022-01-23 15:52:33 +0000

    #1 Fix registration so it works with first and last name

---
+++ tests/Unit/UserTest.php
@@ -0,0 +1,10 @@
+<?php
+
+use App\Models\User;
+
+// test('a user has a profile', function () {
+//     // Given
+//     $user = User::factory()->create();
+
+//     $this->assertInstanceOf(Collection::class, $user->profile);
+// });
